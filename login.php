84e38fa4a3d187213866d7dd34f5ae17075ed07c
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-03-05 15:07:24 +0500

    create login page and add SecurityManager.js

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<script src="SecurityManager.js"></script>
+<head>
+
+<body>
+    <div style="border:1px solid red; width:300px; float:left;">
+        <h1>Admin Login</h1>
+            <div>
+                Login: <input id="txtLogin" /> <br>
+                Password: <input id="txtPassword" type="password" /> <br>
+                <input type="submit" id="btnLogin" value="Login" />
+                <input type="reset" value="cancel" />
+            </div>
+    </div>
+    <div style="border:1px solid red; width:300px; float:left;margin-left:20px;">
+        <h1>User Login</h1>
+            <div>
+                Login: <input id="txtLogin1" /> <br>
+                Password: <input id="txtPassword1" type="password" /> <br>
+                <input type="submit" id="btnLogin1" value="Login" />
+                <input type="reset" value="cancel" />
+            </div>
+    </div>
+
+</body>
+</html>
