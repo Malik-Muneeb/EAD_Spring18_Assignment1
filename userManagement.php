9d25e938da7749425679f6f9ec6e850308473bee
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-03-07 00:35:56 +0500

    handled validation

--- userManagement.php
+++ userManagement.php
@@ -29,6 +29,36 @@ function main() {
         }   
     }//end of onchange
 
+    function grid() {
+        var userObjArr=SecurityManager.GetAllUsers();   
+        var table = document.createElement("TABLE");
+        table.setAttribute("id", "myTable");
+        document.body.appendChild(table);
+
+        var row = document.createElement("TR");
+        row.setAttribute("id", "myTr");
+        document.getElementById("myTable").appendChild(row);
+        
+        var heading = document.createElement("TH");
+        heading.setAttribute("id", "myTh");
+        document.getElementById("myTable").appendChild(heading);
+
+      
+        for(var i=0; i<userObjArr.length; i++)
+        {    
+              var row = document.createElement("TR");
+        row.setAttribute("id", "myTr");
+        document.getElementById("myTable").appendChild(row);
+
+        var z = document.createElement("TD");
+        var t = document.createTextNode("cell");
+        z.appendChild(t);
+        document.getElementById("myTr").appendChild(z);
+           console.log(userObjArr[i].name);
+        }
+    }
+
+    var gridFunc=grid;
     var btnSave=document.getElementById("btnSave");
     btnSave.onclick=function() {
         var userObjArr=SecurityManager.GetAllUsers();
@@ -43,19 +73,44 @@ function main() {
         console.log(txtLogin);
         console.log(txtPassword);
         console.log(txtName);
+        console.log(txtEmail);
+        console.log(countryName);
         console.log(cityName);
-        console.log(cityName);
-        console.log(cityName);
+        var userObj=new Object();
+        userObj.login=txtLogin;
+        userObj.password=txtPassword;
+        userObj.name=txtName;
+        userObj.email=txtEmail;
+        userObj.country=countryName;
+        userObj.city=cityName;
+        var isLoginExist=false;
+        var isEmailExist=false;
         if(userObjArr.length==0)
         {
-
+            var check=SecurityManager.SaveUser(userObj);
+            console.log(check);
         }
         else 
         {
-        for(var i=0; i<userObjArr.length; i++)
-        {
-            console.log(userObjArr[i]);
-        }
+            for(var i=0; i<userObjArr.length && !isLoginExist && !isEmailExist ; i++)
+            {    
+                if(userObjArr[i].login==userObj.login)
+                    isLoginExist=true;
+                else if(userObjArr[i].email==userObj.email)
+                    isEmailExist=true; 
+                console.log(userObjArr[i].name);
+            }
+            if(isLoginExist)
+                alert("Login Already Existed. Try another One!");
+            else if(isEmailExist)
+                alert("Email Already Existed. Try another one!");
+            else
+            {
+                var check=SecurityManager.SaveUser(userObj);
+                console.log(check);
+                if(check==true)
+                    alert("User Added Successfully!");
+            }
         }
     }
 }
@@ -76,7 +131,7 @@ function main() {
     <div>
         <h1>Users</h1>
         <span>Login: </span> <input type="text" id="txtLogin" ><br>
-        <span>Password: </span> <input type="text" id="txtPassword" ><br>
+        <span>Password: </span> <input type="password" id="txtPassword" ><br>
         <span>Name: </span> <input type="text" id="txtName" ><br>
         <span>Email: </span> <input type="text" id="txtEmail" ><br>
         <span>Country: </span> <select name="" id="cmbCountries"><option>--Select--</option></select><br>
