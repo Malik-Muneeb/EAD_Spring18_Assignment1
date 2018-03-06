1dcb661f359aacd713be30021d51cc0ae2c52a10
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-03-06 17:57:00 +0500

    Change style file name and write html of adminHome page

---
+++ adminHome.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title> Home </title>
+<link href="styles.css" rel="stylesheet">
+<script src="SecurityManager.js"></script>
+<script>
+function main() {
+
+   
+    }
+}
+</script>
+<head>
+
+<body onload="main();">
+    <div  class="menu">
+        <a href="#" >Home</a>
+        <a href="#" >User Management</a>
+        <a href="#" >Role Management</a>
+        <a href="#" >Permissions Management</a>
+        <a href="#" >Role-Permissions Assignment</a>
+        <a href="#" >Useer-Role Assignment</a>
+        <a href="login.php" >Logout</a>
+    </div>
+
+    <div>
+        <h1>Welcome Admin</h1>
+    </div>
+   
+</body>
+</html>
